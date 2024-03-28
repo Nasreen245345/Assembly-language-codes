a1183308cc87b691e4590bd898abfc19fb65b36d
Nasreen <164878074+Nasreen245345@users.noreply.github.com>
2024-03-28 23:06:26 +0500

    Update Task_03.asm
    TITLE: add and subtract
    This program add and subtract number using one register only
    Integers and stores sum in variable.

--- Day_02/Task_03.asm
+++ Day_02/Task_03.asm
@@ -1,1 +1,15 @@
+Include irvine32.inc
+.data
+;variable declarations go here
+.code
+Main Proc
+;instructions go here
+    mov eax,0h 		;initializing eax register to 0h 
+    mov eax,6h		;mov hexadecimal of 6 into eax register
+    sub eax,2h		;subtract hexadecimal of 2 from register eax
+    add eax,5h	        ;add hexadecimal of 5 into eax register
+Call 	dumpregs	;displays the result on the screen by displaying all register values
+Exit
+Main endp
+End main
 
