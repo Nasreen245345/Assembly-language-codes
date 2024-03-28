aa47d65c031729ac51ccdb4381a7c5e9cf399e23
Nasreen <164878074+Nasreen245345@users.noreply.github.com>
2024-03-28 23:07:57 +0500

    Update Task_04.asm
    TITLE: add and subtract
    This program solve the expression 5+(6-2)  using two register only
    Integers and stores sum in variable.

--- Day_02/Task_04.asm
+++ Day_02/Task_04.asm
@@ -1,1 +1,17 @@
+INCLUDE Irvine32.inc
+.data
+    ;variable declaration goes here
+.code
+;instruction goes here
+main PROC
+    mov eax,0h 		;initializing eax register to 0h 
+    mov ebx,0h              ;initializing ebx register to 0h
+    mov ebx,5h		;Mov 5h into ebx register
+    mov eax,6h              ;Mov 6h into eax register
+    sub eax,2h	       ; subtract 2h from eax register
+    add eax,ebx	       ;add value in eax register of ebx 
+    call DumpRegs      ; Display the registers
+exit
+main ENDP
+END main
 
