1040845f998ef4f82ae3b2f2aec558dc44f866ef
Nasreen <164878074+Nasreen245345@users.noreply.github.com>
2024-03-29 12:21:01 +0500

    Update Task_01.asm

--- Day_01/Task_01.asm
+++ Day_01/Task_01.asm
@@ -1,1 +1,16 @@
+Include irvine32.inc
+.data
+;variable declarations go here
+.code
+Main Proc
+;instructions go here
+Mov 	eax, 30		                  ;move 30 into eax register
+Mov 	ebx, 20                     ;move 20 into ebx register
+
+Add 	ecx, eax                    ;Adding content in ecx and eax register
+Add 	ecx, ebx                    ;Adding content in ecx and ebx register
+Call 	dumpregs	;displays the result on the screen by displaying all register values
+Exit
+Main endp
+End main
 
